f247573af2c900a4a093e23e9a8ac5d0d4039bd5
Tomasz Garbus <tomasz.garbus1@gmail.com>
2023-05-15 12:15:21 +0200

    [RuneDroid] List all exercise names

--- RuneDroid/ListOfExercises.tsx
+++ RuneDroid/ListOfExercises.tsx
@@ -1,21 +1,58 @@
 import {
     FlatList,
+    ImageBackground,
     ScrollView,
+    StyleSheet,
     Text
 } from 'react-native';
 import Exercises from './Exercises.json';
 
 function ListOfExercises(): JSX.Element {
-    return (
-        <ScrollView>
-            <Text>text</Text>
-            {
-                Exercises.map(
-                    exercise) => <Text>text</Text>
-                );
-            }
-        </ScrollView>
-    )
+  return (
+    <ScrollView>
+      <Text>text</Text>
+      {
+        Exercises.map(
+          exercise => (
+            <ImageBackground
+              source={require('images/' + exercise.img)}
+              style={styles.image}>
+              <Text
+                key={exercise.id}
+                style={styles.container}>
+                {exercise.title}
+              </Text>
+            </ImageBackground>
+          )
+        )
+      }
+    </ScrollView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#eaeaea',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
+  // title: {
+  //   marginTop: 16,
+  //   paddingVertical: 8,
+  //   borderWidth: 4,
+  //   borderColor: '#20232a',
+  //   borderRadius: 6,
+  //   backgroundColor: '#61dafb',
+  //   color: '#20232a',
+  //   textAlign: 'center',
+  //   fontSize: 30,
+  //   fontWeight: 'bold',
+  // },
+});
+
 
 export default ListOfExercises;
---
+++ RuneDroid/README.md
@@ -0,0 +1,11 @@
+# RuneDroid
+
+## Running the app locally.
+
+See https://reactnative.dev/docs/environment-setup?guide=native for full instructions.
+
+After the environment has been set up, run the following to start a debugging server connected to the Android Virtual Device:
+
+```
+npx react-native run-android
+```
